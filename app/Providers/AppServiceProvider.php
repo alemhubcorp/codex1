babc6c7ba0d541e95b580243a3480a8621b92ea2
Assar <uniteditcapital@gmail.com>
2026-03-20 11:59:14 +0500

    Force https asset URLs in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,10 @@ class AppServiceProvider extends ServiceProvider
   {
       Schema::defaultStringLength(191);
       Paginator::useBootstrap();
+
+      if (env('FORCE_HTTPS') === 'On') {
+          URL::forceScheme('https');
+      }
   }
 
   /**
